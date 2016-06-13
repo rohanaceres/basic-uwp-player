0fea69662ee54af43850cd1502fc9f4950fe912a
ceres r <ceres r>
2016-06-12 21:03:27 -0300

    :art: :memo: Documentação e nome da música
    - Adiciona documentação no código
    - Adiciona nome da música no display

--- BasicUwpPlayer/App.xaml.cs
+++ BasicUwpPlayer/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace StonePlayer
+namespace BasicUwpPlayer
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- BasicUwpPlayer/MainPage.xaml.cs
+++ BasicUwpPlayer/MainPage.xaml.cs
@@ -13,38 +13,64 @@ using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace StonePlayer
+namespace BasicUwpPlayer
 {
     /// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
 	public sealed partial class MainPage : Page
     {
-        List<IRandomAccessStream> RecordingFiles;
+        /// <summary>
+        /// Lista da arquivos para reprodução.
+        /// </summary>
+        List<IHearable> RecordingFiles;
+        /// <summary>
+        /// Índice (em <see cref="RecordingFiles"/>) do próximo arquivo a ser reproduzido.
+        /// </summary>
         int recordingId;
 
         public MainPage()
         {
             this.InitializeComponent();
         }
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Quando o layout é carregado, esse método é chamado.
+        /// Ele dá início ao player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             this.TurnOn();
         }
+        /// <summary>
+        /// Inicia o player.
+        /// </summary>
         private async void TurnOn()
         {
+            // Mantém o display ligado:
             var displayRequest = new DisplayRequest();
             displayRequest.RequestActive();
+
+            // Carrega as musicas da pasta "recordings"
             this.RecordingFiles = await this.LoadRecordingFiles();
+
+            // Começa a tocar as musicas
             this.Play();
         }
-        private void btnNext_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Chamado quando um dos botões de controle do player (next, back, play)
+        /// é pressionado.
+        /// </summary>
+        /// <param name="sender">Botão de controle.</param>
+        /// <param name="e"></param>
+        private void OnPlayerButtonClick(object sender, RoutedEventArgs e)
         {
+            // Faz o controle de play/pause:
             if (sender == this.btnPlay)
             {
                 if (player.CurrentState == MediaElementState.Playing)
                 {
-                    //this.ImageSource = new BitmapImage(new Uri("Assets/pause.png", UriKind.Relative));
                     player.Pause();
                 }
                 else
@@ -52,6 +78,8 @@ namespace StonePlayer
                     player.Play();
                 }
             }
+
+            // Passa para a próxima música:
             else if (sender == this.btnNext)
             {
                 if (this.recordingId < this.RecordingFiles.Count - 1)
@@ -62,6 +90,8 @@ namespace StonePlayer
 
                 this.Play();
             }
+
+            // Volta para a musica anterior:
             else if (sender == this.btnPrev)
             {
                 if (this.recordingId > 0)
@@ -73,43 +103,49 @@ namespace StonePlayer
                 this.Play();
             }
         }
+        /// <summary>
+        /// Toca uma musica.
+        /// </summary>
         private void Play()
         {
+            // Mantém o display ativo:
             var displayRequest = new DisplayRequest();
             displayRequest.RequestActive();
-            this.txtSongTitle.Text = "Gravação " + (this.recordingId + 1);
-            this.player.SetSource(this.RecordingFiles.ElementAt(this.recordingId), ".wav");
+
+            // Define o título da música:
+            this.txtSongTitle.Text = this.RecordingFiles.ElementAt(this.recordingId).Name;
+
+            // Pega a musica a ser reproduzida:
+            this.player.SetSource(this.RecordingFiles.ElementAt(this.recordingId).File, ".mp3");
+            
+            // Toca a música:
             this.player.Play();
         }
-        private async Task<List<IRandomAccessStream>> LoadRecordingFiles()
+        /// <summary>
+        /// Carrega todos os arquivos da pasta "recordings".
+        /// </summary>
+        /// <returns>Retorna uma lista de streams contendo os audios da pasta "recordings".</returns>
+        private async Task<List<IHearable>> LoadRecordingFiles()
         {
             recordingId = 0;
 
-            List<IRandomAccessStream> streams = new List<IRandomAccessStream>();
+            List<IHearable> streams = new List<IHearable>();
 
             StorageFolder appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
             StorageFolder folder = await appInstalledFolder.GetFolderAsync(@"Assets\recordings");
 
             foreach (StorageFile file in await folder.GetFilesAsync())
             {
-                streams.Add(await file.OpenAsync(FileAccessMode.Read));
+                streams.Add(new Hearable(file.Name.Split('.').FirstOrDefault(), await file.OpenAsync(FileAccessMode.Read)));
             }
 
             return streams;
         }
-        private void MusicPlayer_CurrentStateChanged(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine(this.player.CurrentState);
-        }
-        private string GetNextRecordingName()
-        {
-            string recordingName;
-
-            Random recordingId = new Random();
-            recordingName = "Assets/recordings/" + recordingId.Next(1, 7) + ".wav";
-
-            return recordingName;
-        }
+        /// <summary>
+        /// Chamado quando o audio chegou ao fim.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void player_MediaEnded(object sender, RoutedEventArgs e)
         {
             if (this.recordingId < this.RecordingFiles.Count - 1)
@@ -119,6 +155,12 @@ namespace StonePlayer
             else { this.recordingId = 0; }
 
             this.Play();
+        }
+
+        // Just for debug:
+        private void MusicPlayer_CurrentStateChanged(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(this.player.CurrentState);
         }
     }
 }
